29e9128bcdd0fdb21e7cb377892ada6a1482b52c
Kichiyaki <xyztojajestem@gmail.com>
2021-05-02 10:41:02 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# goutil
+
+A package with utility functions that I use in my projects.
---
+++ strutil/is_email.go
@@ -0,0 +1,29 @@
+package strutil
+
+import (
+	"net"
+	"regexp"
+	"strings"
+)
+
+const (
+	minEmailLength = 3
+	maxEmailLength = 254
+)
+
+var emailRegex = regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+\\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+
+func IsEmail(e string) bool {
+	if len(e) < minEmailLength && len(e) > maxEmailLength {
+		return false
+	}
+	if !emailRegex.MatchString(e) {
+		return false
+	}
+	parts := strings.Split(e, "@")
+	mx, err := net.LookupMX(parts[1])
+	if err != nil || len(mx) == 0 {
+		return false
+	}
+	return true
+}
