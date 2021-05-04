3d843a787db7dafd6885382cff39dc366caa2ec0
Kichiyaki <xyztojajestem@gmail.com>
2021-05-04 15:26:59 +0200

    simplify strutil.Underscore

--- strutil/underscore.go
+++ strutil/underscore.go
@@ -2,60 +2,18 @@ package strutil
 
 import (
 	"unicode"
-	"unicode/utf8"
 )
 
-type underscoreBuffer struct {
-	r         []byte
-	runeBytes [utf8.UTFMax]byte
-}
-
-func (b *underscoreBuffer) write(r rune) {
-	if r < utf8.RuneSelf {
-		b.r = append(b.r, byte(r))
-		return
-	}
-	n := utf8.EncodeRune(b.runeBytes[0:], r)
-	b.r = append(b.r, b.runeBytes[0:n]...)
-}
-
-func (b *underscoreBuffer) indent() {
-	if len(b.r) > 0 {
-		b.r = append(b.r, '_')
-	}
-}
-
 func Underscore(s string) string {
-	b := underscoreBuffer{
-		r: make([]byte, 0, len(s)),
-	}
-	var m rune
-	var w bool
-	for _, ch := range s {
-		if unicode.IsUpper(ch) {
-			if m != 0 {
-				if !w {
-					b.indent()
-					w = true
-				}
-				b.write(m)
+	r := ""
+	for i, c := range s {
+		if unicode.IsUpper(c) {
+			if i > 0 && i+1 < len(s) && (unicode.IsLower(rune(s[i-1])) || unicode.IsLower(rune(s[i+1]))) {
+				r += "_"
 			}
-			m = unicode.ToLower(ch)
-		} else {
-			if m != 0 {
-				b.indent()
-				b.write(m)
-				m = 0
-				w = false
-			}
-			b.write(ch)
+			c = unicode.ToLower(c)
 		}
+		r += string(c)
 	}
-	if m != 0 {
-		if !w {
-			b.indent()
-		}
-		b.write(m)
-	}
-	return string(b.r)
+	return r
 }
